e481352ced21eac79f643c7f33c6a4fde3d59b2a
Jaspreet Singh <jaspreetsidhu3may@gmail.com>
2023-10-13 01:49:02 +0530

    style refinements

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 var space=document.getElementsByClassName('space');
+var homePageImage=document.getElementById('homepageImage');
+homePageImage.classList.remove('fadeout')
+
+
 for (var i = 0; i < space.length; i++) {
-    space.item(i).innerText="..................";
+    space.item(i).innerText="---------------------";
  }
+
+function openNav() {
+    document.getElementById("navSupportContent").style.width = "250px";
+    homePageImage.classList.add('fadeout');
+}
+  
+function closeNav() {
+    document.getElementById("navSupportContent").style.width = "0";
+    homePageImage.classList.remove('fadeout')
+}
