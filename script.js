c64a4c5711b753ba9bbb30603bc96e3111c5bdf7
Jaspreet Singh <jaspreetsidhu3may@gmail.com>
2024-01-07 21:18:07 -0500

    added transition

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ navToggle.addEventListener("click", function(){
     var logo=document.getElementById('logo_web');
     if(toggleCount%2==0){
         logo.style.top="50%";
+        logo.style.transition="all 0.4s ease";
     }
     else{
         logo.style.top="25%";
+        logo.style.transition="all 0.4s ease";
     }
     toggleCount++;
 });
